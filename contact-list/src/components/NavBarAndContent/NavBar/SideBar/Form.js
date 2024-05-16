5d9c69a53a80e684112f91c24761b8732d8e3c51
iQily <rifqi.aly@gmail.com>
2024-05-16 22:48:33 +0700

    sidebar with animation Done

--- contact-list/src/components/NavBarAndContent/NavBar/SideBar/Form.js
+++ contact-list/src/components/NavBarAndContent/NavBar/SideBar/Form.js
@@ -1,8 +1,44 @@
 import React from 'react'
 import './Form.css'
+import Lottie from 'react-lottie';
+import animationData from '../../../../assets/animation.json'
+import Button from '../../../UI/Button';
 const Form = () => {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+
     return (
-        <div>Form</div>
+        <form className='form'>
+            <div className='add-new-img'>
+                <Lottie
+                    options={defaultOptions}
+                    height={140}
+                    width={140}
+                />
+
+            </div>
+            <div className='input-text'>
+                <input
+                    type='text'
+                    placeholder='Name' />
+                <input
+                    type='text'
+                    placeholder='Surname' />
+            </div>
+            <div className='input-tel'>
+                <input
+                    type='text'
+                    placeholder='123456789034567' />
+            </div>
+            <Button name='Add' />
+        </form>
     )
 }
 
